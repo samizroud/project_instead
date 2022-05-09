63af20438fa634cd8a22eca591b72f798bf55b0e
sami zroud <sami.zroud@gmail.com>
2022-05-09 18:03:40 +0100

    création de layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,49 +8,47 @@ import { ListItem } from './list/list.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  public rulesTitle: string = 'The goal is not only to create a working example but well written code:';
+  // public rulesTitle: string = 'The goal is not only to create a working example but well written code:';
 
   public rules: ListItem[] = [
     {
-      lines: ['Create reusable, configurable and performant components'],
+      lines: ['tag','tag2'],
     },
-    {
-      lines: ['Write well typed code'],
-    },
-    {
-      lines: ['Adapt to the existing code-style'],
-    },
-    {
-      lines: ['Comment your code and explain your choices'],
-    },
+   
+    // {
+    //   lines: ['Adapt to the existing code-style'],
+    // },
+    // {
+    //   lines: ['Comment your code and explain your choices'],
+    // },
   ];
 
   public instructionsTitle: string = 'Given the limited time available, implement in priority order:';
 
   public instructions: ListItem[] = [
     {
-      lines: ['General layout'],
+      lines: ['tag'],
     },
-    {
-      lines: ['Possibility to rename the Master tag'],
-    },
-    {
-      lines: ['Possibility to add and remove tags'],
-    },
-    {
-      lines: ['Possibility to name the tag to be added'],
-    },
-    {
-      lines: ['Imagine that performances might be a concern'],
-    },
-    {
-      lines: [
-        'The +n behaviour when the space is too narrow to show every tags',
-        '(Think of all the ways for the available space to change)',
-      ],
-    },
-    {
-      lines: ['Now, tags come from a (slow) api'],
-    },
+    // {
+    //   lines: ['Possibility to rename the Master tag'],
+    // },
+    // {
+    //   lines: ['Possibility to add and remove tags'],
+    // },
+    // {
+    //   lines: ['Possibility to name the tag to be added'],
+    // },
+    // {
+    //   lines: ['Imagine that performances might be a concern'],
+    // },
+    // {
+    //   lines: [
+    //     'The +n behaviour when the space is too narrow to show every tags',
+    //     '(Think of all the ways for the available space to change)',
+    //   ],
+    // },
+    // {
+    //   lines: ['Now, tags come from a (slow) api'],
+    // },
   ];
 }
